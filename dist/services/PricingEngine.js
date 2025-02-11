33b1fdc928a7c8c84d97059287038794675c1766
iheb <iheb.jendoubi@atlasenergies.fr>
2025-02-11 20:02:18 +0000

    structure refactoring

--- dist/services/PricingEngine.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.calculateFinalQuote = void 0;
-const conditionEvaluator_1 = require("../utils/conditionEvaluator");
-const operationExecutor_1 = require("../utils/operationExecutor");
-const calculateFinalQuote = (product, customerData) => product.rules.reduce((quote, rule) => (0, conditionEvaluator_1.evaluateConditions)(customerData, rule.conditions) ? (0, operationExecutor_1.applyOperation)(quote, rule.operation) : quote, product.basePrice);
-exports.calculateFinalQuote = calculateFinalQuote;
